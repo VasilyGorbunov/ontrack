cf30d8d85c933af7411175a7089bba7c3260d058
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-01-13 14:45:19 +0300

    09 - done

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -2,14 +2,24 @@
 import {ref} from "vue";
 import {ChartBarIcon, ClockIcon, ListBulletIcon} from "@heroicons/vue/24/outline";
 import NavItem from "@/components/NavItem.vue";
+import {PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE} from "@/constants.js";
 
 const navItems = {
-  timeline: ClockIcon,
-  activities: ListBulletIcon,
-  progress: ChartBarIcon,
+  [PAGE_TIMELINE]: ClockIcon,
+  [PAGE_ACTIVITIES]: ListBulletIcon,
+  [PAGE_PROGRESS]: ChartBarIcon,
 }
 
-const currentPage = ref('timeline')
+function normalizePageHash() {
+  const hash = window.location.hash.slice(1)
+  if (Object.keys(navItems).includes(hash)) {
+    return hash
+  }
+  window.location.hash = PAGE_TIMELINE
+  return PAGE_TIMELINE
+}
+
+const currentPage = ref(normalizePageHash())
 </script>
 
 <template>
