0c8eb61ada95054f54d4c94b62d1d5e3aceaf8ee
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-10-23 11:59:56 +0300

    part-05

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup>
 import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+import {
+  ClockIcon,
+  ListBulletIcon,
+  ChartBarIcon,
+} from '@heroicons/vue/24/outline'
+
+const navItems = {
+  timeline: ClockIcon,
+  activities: ListBulletIcon,
+  progress: ChartBarIcon
+}
 </script>
 
 <template>
-  <header
-    class="sticky top-0 z-20 flex items-center justify-between p-3 bg-white border-b"
-  >
+  <header class="sticky top-0 z-20 flex items-center justify-between p-3 bg-white border-b">
     <a href="#">
       <img src="./assets/img/logo.png" alt="Logo" class="h-9" />
     </a>
@@ -20,5 +29,14 @@ import { CheckCircleIcon } from '@heroicons/vue/24/solid'
       </div>
     </a>
   </header>
-  <main></main>
+  <main class="flex flex-col flex-grow"></main>
+  <nav class="sticky bottom-0 bg-white z-10">
+    <ul class="flex items-center justify-around border-t">
+      <li :key="page" v-for="icon, page in navItems" class="flex-1">
+        <a class="flex flex-col capitalize items-center p-2 text-xs" :href="`#${page}`">
+          <component :is="icon" class="size-6" /> {{ page }}
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
